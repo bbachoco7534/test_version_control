0a0a1e2bd62e73ab6c61eb0a8d5afe7aef9feb0b
Brandon Bachoco <bbachoco7534@floridapoly.edu>
2025-11-12 23:48:29 +0000

    First commit of player input

--- game.py
+++ game.py
@@ -1,1 +1,7 @@
 print("Welcome to Rock-Paper-Scissors!")
+
+choices = ["rock", "paper", "scissors"]
+
+player_input = input("Choose rock, paper, or scissors: ").lower()
+
+print(f"You chose: {player_input} ")
